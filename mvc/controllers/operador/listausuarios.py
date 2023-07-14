172039421a0ba706362a51484dc32191ae8fe7e0
AdriLeon <51243138+AdriLeon@users.noreply.github.com>
2023-07-14 00:54:50 -0600

    Lista usuarios administrador y operador funciona

--- mvc/controllers/operador/listausuarios.py
+++ mvc/controllers/operador/listausuarios.py
@@ -1,8 +1,17 @@
 import web
-import app
+import pyrebase
+import firebase_config as token
+import app as app
+import json
 
 render = web.template.render("mvc/views/operador/") #ruta de las vistas
 
 class ListaUsuarios: #clase Index
     def GET(self):
-        return render.lista_usuarios()
+        try:
+            firebase = pyrebase.initialize_app(token.firebaseConfig)
+            db = firebase.database()
+            users = db.child("data").child("usuarios").get()
+            return render.lista_usuarios(users)
+        except Exception as error:
+            print("Error UsersList.GET: {}".format(error))
