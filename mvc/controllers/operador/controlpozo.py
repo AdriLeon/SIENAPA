dd5edcea0b4d0a22fdcd7c685643c52887e42706
luisgar289 <1720110205@utectulancingo.edu.mx>
2023-07-21 17:35:26 -0600

    Fixed

--- mvc/controllers/operador/controlpozo.py
+++ mvc/controllers/operador/controlpozo.py
@@ -15,14 +15,4 @@ class ControlPozo: #clase Index
         cookie = web.cookies().get("localid") #almacena los datos de la cookie
         users = db.child('data').child('usuarios').get()
         pozos = db.child('data').child('pozos').get()
-        for user in users.each():
-            if user.key() == cookie and user.val()['nivel'] == 'administrador':
-                return web.seeother('/logout')
-            elif user.key() == cookie and user.val()['nivel'] == 'operador':
-                web.setcookie('localid', None)
-                return render.control_pozo(pozos)
-            elif user.key() == cookie and user.val()['nivel'] == 'informatica':
-                web.setcookie('localid', None)
-                return web.seeother('/logout')
-        web.setcookie('localid', None)
-        return web.seeother('/logout')
+        return render.control_pozo(pozos)
