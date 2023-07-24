e482f336d48bcd7f32d1fbacd8b5d65202c05ad1
AdriLeon <51243138+AdriLeon@users.noreply.github.com>
2023-07-24 15:48:01 -0600

    .

--- mvc/controllers/admin/listapozos.py
+++ mvc/controllers/admin/listapozos.py
@@ -6,11 +6,8 @@ import json
 from datetime import datetime
 
 render = web.template.render("mvc/views/admin/") #ruta de las vistas
-firebase = pyrebase.initialize_app(token.firebaseConfig)
-auth = firebase.auth() #verifica la conexión a firebase
-db = firebase.database()
+
 
 class ListaPozos: #clase Index
     def GET(self):
-        pozos = db.child('data').child('pozos').get()
-        return render.lista_pozos(pozos)
+        return render.lista_pozos()
